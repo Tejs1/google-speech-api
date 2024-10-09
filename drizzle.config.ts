501d126a30862f897d3c68b81b7bd8fbf79d8930
Tejs1 <tejsrelax@gmail.com>
2024-10-09 18:39:04 +0530

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["tts-stt-api_*"],
+} satisfies Config;
